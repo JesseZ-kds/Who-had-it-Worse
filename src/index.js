4bcb1feab510b1362bc5c765c8d6a36807589f85
JesseZ-kds <jesse.zelman@kds.vic.edu.au>
2024-05-27 09:45:48 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+const left = document.getElementById('left');
+const leftApp = new PIXI.Application();
+let leftGraphics = new PIXI.Graphics();
 
+const right = document.getElementById('right');
+const rightApp = new PIXI.Application();
+let rightGraphics = new PIXI.Graphics();
+
+left.appendChild(leftApp.view);
+leftApp.stage.addChild(leftGraphics);
+
+right.appendChild(rightApp.view);
+rightApp.stage.addChild(rightGraphics);
+
+//
+
+let frames = require('./framse.json')
+let availableFrames = Object.keys(frames);
